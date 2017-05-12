9ec4c47c7ff3c8a175d04a183855dab05de9da37
jyang8 <jyang8@stuy.edu>
2017-05-12 12:13:06 -0400

    initial files

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, session, redirect, url_for, request
+from utils import analyze
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route("/train/", methods = ['GET', 'POST'])
+def train():
+    return render_template('train.html')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
