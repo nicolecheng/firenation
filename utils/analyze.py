c150a519a7efd304ea29ec27f73963bafd622ab4
nicolecheng <ncheng@stuy.edu>
2017-05-21 17:25:19 -0400

    wrote all_trains() and get_train(number,direction)

--- utils/analyze.py
+++ utils/analyze.py
@@ -28,21 +28,21 @@ trip_update {
 def get_stop_id(entity):
     return entity.trip_update.stop_time_update[0].stop_id
 
-e = feed.entity[0]
-print "Stop ID: " + str(get_stop_id(e))
+#e = feed.entity[0]
+#print "Stop ID: " + str(get_stop_id(e)) 
 
 # returns the trip id for a given entity which can be used to identify the train
 def get_trip_id(entity):
     return entity.trip_update.trip.trip_id
 
-print "Trip ID: " + str(get_trip_id(e))
+#print "Trip ID: " + str(get_trip_id(e))
 
 # returns the trip id for a given entity which can be used to identify the train
 def get_train(entity):
     trip_id = get_trip_id(entity)
     return trip_id[7]
 
-print "Train: " + str(get_train(e))
+#print "Train: " + str(get_train(e))
 
 '''
 # sample call
@@ -68,5 +68,38 @@ def get_station_name(stop_id):
     d = json.loads(page)
     return d["result"]["name"]
 
-sid = get_stop_id(e)
-print "Station Name: " + str(get_station_name(sid))
+#sid = get_stop_id(e)
+#print "Station Name: " + str(get_station_name(sid))
+
+#[[train,[stops]],[train,[stops]],...]
+alltrains = []
+
+# updates global list alltrains, containing all trains
+def all_trains():    
+    for i in feed.entity:
+        if(len(str(i.trip_update.trip.trip_id))>1):
+            train=i.trip_update.trip.trip_id
+            stops=[]
+            for s in i.trip_update.stop_time_update:
+                stops.append(s.stop_id)
+            alltrains.append([train,stops])
+    #print alltrains
+    #print feed.entity[0].trip_update.trip.trip_id
+
+all_trains()
+
+#print get_station_name(alltrains[100][1][0])
+
+# returns a list of all stops that the trains are at
+# i.e. (3,'N') = 3 train, northbound
+def get_train(number,direction):
+    stop_names=[]
+    for t in alltrains:
+        if "_"+str(number)+".."+str(direction) in t[0]:
+            for s in t[1]:
+                m = get_station_name(s)
+                if m not in stop_names:
+                    stop_names.append(m)
+    return stop_names
+
+print get_train(1,'S')
