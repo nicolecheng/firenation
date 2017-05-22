f805ba6e70fd18ada03295c93cb2c10adaf8fa46
getDolla <ywang8@stuy.edu>
2017-05-21 23:31:49 -0400

    fixed 404

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
     return render_template('home.html')
 
 @app.route("/<train_name>/", methods = ['GET', 'POST'])
-def train():
+def train(train_name):
     return render_template('train.html')
 
 if __name__ == "__main__":
