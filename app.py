5327300cf72c2163dfda6e807c1fbf8798964063
CH33ZED <zlin3@stuy.edu>
2017-05-24 22:56:32 -0400

    stuff

--- app.py
+++ app.py
@@ -12,8 +12,18 @@ def home():
 def train(train_name):
     stops = analyze.get_stops(train_name)
     return render_template('train.html', stops = stops)
+''' 
+    info = {}
+    for a in stops{
+         direction = analyze.get_direction(a)
+         arrivaltime = analyze.get_arrival_time(a)
+         info[a] = infu
+    }
+'''
+    
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
 
+    
