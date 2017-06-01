22f0ebfd9381d5c31a08139867b00ee6e1e5fd46
getDolla <ywang8@stuy.edu>
2017-06-01 16:11:58 -0400

    gps location works

--- static/gps.js
+++ static/gps.js
@@ -1,14 +1,11 @@
-var x = document.getElementById("demo");
-
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
+    } else {
+        alert("Geolocation is not supported by this browser.");
     }
 }
 
 function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
+    window.location = "train.html/" + position.coords.latitude + "+" + position.coords.longitude;
 }
