e0389402e14609321b4e7654d59aca553727cfbf
Yikai <ywang8@stuy.edu>
2017-06-02 11:56:03 -0400

    updated data.js

--- static/data.js
+++ static/data.js
@@ -1,9 +1,10 @@
 // Initialization
+var station_list = document.getElementsByClassName("station");
 
 var result = [];
-// Data Retrieval - By Year
-var get_data = function(year) {
-    var ret;
+// Data Retrieval
+var get_data = function(station) {
+
     $.ajax({
 	url: '/data/' + year,
 	dataType: 'json',
@@ -13,11 +14,11 @@ var get_data = function(year) {
 	    console.log(result);
 	}
     });
+    
     return result;
 };
 
-var station_list = document.getElementsByClassName("station");
-
 for( var i = 0; i < station_list.length; i++ ) {
   console.log(station_list[i].id);
+
 }
